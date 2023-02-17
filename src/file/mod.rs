a87d5406476690bd444387c0f43e3e180147abe1
Hannes Roever <h.roever@zertificon.com>
2023-02-17 18:54:16 +0100

    - fix missing endpoints

--- src/file/mod.rs
+++ src/file/mod.rs
@@ -4,14 +4,9 @@ use std::error::Error;
 use postgres::*;
 use tokio_postgres::{NoTls};
 
-
-
-//static PG_CONNECTION_STRING: &str =  "host=localhost user=postgres pass=mysecretpassword ";
 static PG_CONNECTION_STRING: &str =  "postgresql://postgres:mysecretpassword@192.168.176.2/threedeefilesmanagement";
 
 pub async fn get_all_files() -> Result<Vec<common::File>, Box<dyn Error>> {
-    //let mut client = Client::connect(PG_CONNECTION_STRING, NoTls)?;
-
     let (client, connection) = tokio_postgres::connect(PG_CONNECTION_STRING, NoTls).await?;
 
     tokio::spawn(async move {
@@ -37,10 +32,18 @@ pub async fn get_all_files() -> Result<Vec<common::File>, Box<dyn Error>> {
     Ok(vec)
 }
 
-pub fn get_single_file(id: u32) -> Result<Vec<common::File>, Box<dyn Error>> {
-    let mut client = Client::connect(PG_CONNECTION_STRING, NoTls)?;
+pub async fn get_single_file(id: u32) -> Result<Vec<common::File>, Box<dyn Error>> {
+    let (client, connection) = tokio_postgres::connect(PG_CONNECTION_STRING, NoTls).await?;
+
+    tokio::spawn(async move {
+        if let Err(e) = connection.await {
+            eprintln!("connection error: {}", e);
+        }
+    });
+
+    let query = format!("SELECT \"Id\", \"Name\", \"Author\", \"Created\", \"Size\", \"Downloads\", \"AverageRating\" FROM file WHERE \"Id\" = {};", id);
     let mut vec:Vec<common::File> = Vec::new();
-    for row in &client.query("SELECT Id, Name, Author, Created, Size, Downloads, AverageRating FROM file WHERE Id = 1;", &[]).unwrap() {
+    for row in client.query(&*query, &[]).await? {
         let person = common::File {
             pk_id: row.get(0),
             name: row.get(1),
@@ -55,10 +58,19 @@ pub fn get_single_file(id: u32) -> Result<Vec<common::File>, Box<dyn Error>> {
     Ok(vec)
 }
 
-pub fn get_file_history(file_fk: u32) -> Result<Vec<common::FileHistory>, Box<dyn Error>> {
-    let mut client = Client::connect(PG_CONNECTION_STRING, NoTls)?;
+pub async fn get_file_history(file_fk: u32) -> Result<Vec<common::FileHistory>, Box<dyn Error>> {
+    let (client, connection) = tokio_postgres::connect(PG_CONNECTION_STRING, NoTls).await?;
+
+    tokio::spawn(async move {
+        if let Err(e) = connection.await {
+            eprintln!("connection error: {}", e);
+        }
+    });
+
+    let query = format!("SELECT \"Id\", \"ChangedOn\", \"ByAuthor\", \"State\", \"Content\" FROM filehistory WHERE \"File_fk\" = {};", file_fk);
+
     let mut vec = Vec::new();
-    for row in &client.query("SELECT Id, ChangedOn, ByAuthor, State, Content FROM FileHistory WHERE File_fk = 1;", &[]).unwrap() {
+    for row in &client.query(&*query, &[]).await? {
         let person = common::FileHistory {
             pk_id: row.get(0),
             changed: row.get(1),
--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,6 @@ use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 use std::env;
 
 
-fn get_database_url() -> String {
-    let key = "DATABASE_URL";
-    match env::var(key) {
-        Ok(val) => val,
-        _ => "127.0.0.1".to_string()
-    }
-}
-
-//#[actix_web::main]
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
@@ -28,7 +19,7 @@ async fn main() -> std::io::Result<()> {
 
 #[get("/")]
 async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("3D Files Management API v1")
+    HttpResponse::Ok().body("3D Files Management APIv1")
 }
 
 #[get("/files")]
@@ -44,7 +35,8 @@ async fn get_files() -> impl Responder {
 
 #[get("/files/{id}")]
 async fn get_file(path: web::Path<(u32,)>) -> impl Responder {
-    let file = file::get_single_file(path.into_inner().0).expect("File not found. error during query");
+    let file = file::get_single_file(path.into_inner().0)
+        .await.expect("File not found. error during query");
     if file.is_empty(){
         HttpResponse::NotFound().json("Files not found")
     }
@@ -55,7 +47,8 @@ async fn get_file(path: web::Path<(u32,)>) -> impl Responder {
 
 #[get("/files/{id}/history")]
 async fn get_file_history(path: web::Path<(u32,)>) -> impl Responder {
-    let file_history = file::get_file_history(path.into_inner().0).expect("Error during query");
+    let file_history = file::get_file_history(path.into_inner().0)
+        .await.expect("Error during query");
     if file_history.is_empty(){
         HttpResponse::NotFound().json("File History not found")
     }
@@ -63,36 +56,3 @@ async fn get_file_history(path: web::Path<(u32,)>) -> impl Responder {
         HttpResponse::Ok().json(file_history)
     }
 }
-
-//fn read_files_from_db(query: String) -> std::result::Result<Vec<file>, Box<dyn std::error::Error>> {
-//    let url = &*get_database_url();
-//    //let url = &*format!("mysql://root:example@{}:3306/ThreeDeeFilesManagement", endpoint);
-//    //println!("{}", url);
-//    let pool = Pool::new(url)?;
-//    let mut conn = pool.get_conn()?;
-//    let files:Vec<file> = conn
-//        .query_map(
-//            query,
-//            |(pk_id, name, author, created, size, downloads, rating)| {
-//                file { pk_id, name, author, created, size, downloads, rating }
-//            },
-//        )?;
-//    Ok(files)
-//}
-//
-//fn read_file_history_from_db(query: String) -> std::result::Result<Vec<FileHistory>, Box<dyn std::error::Error>> {
-//    let url = &*get_database_url();
-//    //let url = &*format!("mysql://root:example@{}:3306/ThreeDeeFilesManagement", endpoint);
-//    let pool = Pool::new(url)?;
-//    let mut conn = pool.get_conn()?;
-//
-//    let selected_payments:Vec<FileHistory> = conn
-//        .query_map(
-//            query,
-//            |(pk_id, changed, author, state, content)| {
-//                FileHistory {pk_id, changed, author, state, content }
-//            },
-//        )?;
-//
-//    Ok(selected_payments)
-//}
